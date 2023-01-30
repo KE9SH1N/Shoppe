bda95d7db1e908c5d85afbaf17f9eadd237fe585
KE9SH1N <K3NSHI9@gmail.com>
2023-01-30 17:31:45 +0600

    added navmenu active link status

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const activePage = window.location.pathname;
+
+console.log(activePage);
+
+const navLinks = document.querySelector('nav').querySelectorAll("a").
+
+forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('ct-active');
+    }
+})
