25c41546877fb6af70c1887378cf4f173dc3f524
KE9SH1N <K3NSHI9@gmail.com>
2023-01-31 22:45:50 +0600

    added navmenu active status on product details page

--- assets/js/nav-menu.js
+++ assets/js/nav-menu.js
@@ -5,16 +5,39 @@ const panels = document.querySelectorAll(".section-panel");
 
 projectDetailsMenus.forEach(menu => {
     menu.addEventListener("click", ()=>{
-        let menuTarget = menu.getAttribute("aria-controls")
-
+        let menuTarget = menu.getAttribute("aria-controls");
+        let menuId = menu.getAttribute("id");
+        // console.log(menuId);
+        // console.log(event.target.id);
+        if(event.target.id === menuId){
+            projectDetailsMenus.forEach((item)=>{
+                item.classList.remove("ct-active");
+            })
+            menu.classList.add("ct-active");
+        }
+        else{
+        }
         panels.forEach(panel =>{
-            let panelId = panel.getAttribute("id")
+            let panelId = panel.getAttribute("id");
             if(menuTarget === panelId){
-                panel.classList.remove("hidden")
+                panel.classList.remove("hidden");
             }
             else{
-                panel.classList.add("hidden")
+                panel.classList.add("hidden");
             }
         })
     })
 })
+
+// projectDetailsMenus.forEach(menu =>{
+//     menu.addEventListener("click", ()=>{
+//         let menuNav = menu.getAttribute("id");
+//         console.log(menu);
+//         if(menuNav === menu.getAttribute("id")){
+//             menu.classList.add("ct-active");
+//         }
+//         else{
+//             menu.classList.remove("ct-active");
+//         }
+//     })
+// })
