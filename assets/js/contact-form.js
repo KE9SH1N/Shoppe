820a5054f4c47c1a85e1413a4283489e091a54d4
KE9SH1N <K3NSHI9@gmail.com>
2023-02-02 22:48:21 +0600

    added account page input field cross button functionality

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -17,10 +17,10 @@ function firstNameCross(){
 }
 
 function lastNameCross(){
-    const firstName = document.querySelector("#lastName").value;
+    const lastName = document.querySelector("#lastName").value;
     const crossIcon = document.querySelector("#lName i");
     
-    if(firstName.length <= 0){
+    if(lastName.length <= 0){
         crossIcon.classList.add("hidden");
     }
     else(
@@ -34,20 +34,55 @@ function lastNameCross(){
         
 }
 
-function emailNameCross(){
-    const firstName = document.querySelector("#email").value;
+function emailCross(){
+    const email = document.querySelector("#email").value;
     const crossIcon = document.querySelector("#Email i");
     
-    if(firstName.length <= 0){
+    if(email.length <= 0){
         crossIcon.classList.add("hidden");
+        
     }
-    else(
-        crossIcon.classList.remove("hidden")
-    )
-
+    else{
+        crossIcon.classList.remove("hidden");
+    }
     crossIcon.addEventListener("click", ()=>{
         document.querySelector("#email").value = "";
         crossIcon.classList.add("hidden");
     })
         
+}
+
+
+function regEmailCross(){
+    const regEmail = document.querySelector("#regEmail").value;
+    const crossIconBtn = document.querySelector("#rEmail i");
+
+    if(regEmail.length <= 0 ){
+        crossIconBtn.classList.add("hidden");
+    }
+    else{
+        crossIconBtn.classList.remove("hidden");
+    }
+        
+    crossIconBtn.addEventListener("click", ()=>{
+        document.querySelector("#regEmail").value = "";
+        crossIconBtn.classList.add("hidden");
+    })
+}
+
+function contactNumberCross(){
+    const contact = document.querySelector("#contact").value;
+    const contactCrossBtn = document.querySelector("#Contact i");
+
+    if(contact.length <= 0 ){
+        contactCrossBtn.classList.add("hidden");
+    }
+    else{
+        contactCrossBtn.classList.remove("hidden");
+    }
+        
+    contactCrossBtn.addEventListener("click", ()=>{
+        document.querySelector("#contact").value = "";
+        contactCrossBtn.classList.add("hidden");
+    })
 }
