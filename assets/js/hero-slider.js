f2f1b3f616fd236befd175c91d87f5b44141ee97
KE9SH1N <K3NSHI9@gmail.com>
2023-02-08 20:20:02 +0600

    added user profile on click functionality

--- assets/js/hero-slider.js
+++ assets/js/hero-slider.js
@@ -11,3 +11,23 @@ var swiper = new Swiper(".heroSwiper", {
         el: ".swiper-pagination",
       },
     });
+
+
+
+const userProfile =  document.querySelector("#accountDashboard");
+// console.log(userProfile);
+const dashboardMenu = document.querySelector("#dashboardMenu");
+// console.log(dashboardMenu);
+
+window.addEventListener('click', (e)=>{
+  console.log(e.target);
+  if(e.target.id === "menuOn" || e.target.id === "userProfileImg"){
+    dashboardMenu.classList.toggle("hidden");
+  }
+  else{
+    dashboardMenu.classList.add("hidden");
+  }
+  
+})
+
+
