b6c8058c54d481aee663fecab797b4d21a8dd728
KE9SH1N <K3NSHI9@gmail.com>
2023-02-11 04:12:10 +0600

    added dropdown arrow changing function on cart page

---
+++ assets/js/cart.js
@@ -0,0 +1,7 @@
+function expandShipping(id){
+    upSide = id.querySelector(".fa-angle-up");
+    downSide = id.querySelector(".fa-angle-down");
+    // console.log(downSide);
+    upSide.classList.toggle("hidden");
+    downSide.classList.toggle("hidden");
+}
