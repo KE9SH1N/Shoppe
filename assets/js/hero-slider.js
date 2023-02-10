933eee355132878f187304bdcf2e1c0ca56f75d4
KE9SH1N <K3NSHI9@gmail.com>
2023-02-10 16:20:43 +0600

    added search component and search icon on click functionality  on index page

--- assets/js/hero-slider.js
+++ assets/js/hero-slider.js
@@ -54,7 +54,6 @@ const cartBagCross = document.getElementById("bagCrossBtn");
 const crossBtn = document.querySelector("#crossBtn");
 const productOne = document.querySelector("#product1");
 
-
 cartToggler.addEventListener('click', (e)=>{
   
     if(e.target.id === "cartIcon" || e.target.id === "cartBtnToggler" || e.target.id === "cartNotification"){
@@ -73,5 +72,15 @@ crossBtn.addEventListener("click",(e)=>{
   productOne.classList.add("hidden");
 })
 
+
+function hightSlide(id, hSize) {
+     if (id.classList.contains("h-0")) {
+          id.classList.remove("h-0", "opacity-0", "invisible");
+          id.classList.add(`${hSize}`, "opacity-100", "visible");
+     } else {
+          id.classList.add("h-0", "opacity-0", "invisible");
+          id.classList.remove(`${hSize}`, "opacity-100", "visible");
+     }
+}
 
 
